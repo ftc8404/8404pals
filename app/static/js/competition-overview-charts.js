7168a4fa1215f37336aaf6f60b64a4de9e3eb083
Alex <axchen7@outlook.com>
2018-12-08 22:30:01 -0800

    stuff

--- app/static/js/competition-overview-charts.js
+++ app/static/js/competition-overview-charts.js
@@ -8,19 +8,59 @@ Chart.defaults.global.defaultFontColor.defaultFontFamily = "'Source Sans Pro', '
 Chart.defaults.global.defaultFontColor.defaultFontSize = 16;
 
 var allData = compData.allData;
-var allTeams = [];
+var allTeamsPreGame = [];
+var allTeamsMatch = [];
 var preGameDatasets = [{ label: 'Pre-Game Autonomous', data: [], backgroundColor: '#ff6699', stack: 'pre-game' }, { label: 'Pre-Game Tele-Op', data: [], backgroundColor: '#ffcc66', stack: 'pre-game' }];
 var matchDatasets = [{ label: 'Match Autonomous', data: [], backgroundColor: '#cc99ff', stack: 'main' }, { label: 'Match Tele-Op', data: [], backgroundColor: '#6699ff', stack: 'main' }];
+
+var preGameDataUnsorted = [];
+var matchDataUnsorted = [];
+
+var missingPreGameTeams = [];
+var missingMatchTeams = [];
+
 for (let teamNumber in allData) {
-    allTeams.push(teamNumber);
+
     let teamDataRaw = allData[teamNumber];
+    if (teamDataRaw[32] == 'N/A') {
+        missingPreGameTeams.push(teamNumber)
+    } else {
+        preGameDataUnsorted.push([teamNumber, Math.max(teamDataRaw[29], teamDataRaw[30]), teamDataRaw[32]]);
+    }
 
-    preGameDatasets[0].data.push(Math.max(teamDataRaw[29], teamDataRaw[30]));
-    preGameDatasets[1].data.push(teamDataRaw[32]);
-
-    matchDatasets[0].data.push(teamDataRaw[34]);
-    matchDatasets[1].data.push(teamDataRaw[35]);
+    if (teamDataRaw[35] == 'N/A') {
+        missingMatchTeams.push(teamNumber)
+    } else {
+        matchDataUnsorted.push([teamNumber, teamDataRaw[34], teamDataRaw[35]]);
+    }
 }
+
+preGameDataUnsorted.sort(function (a, b) { return (b[1] + b[2]) - (a[1] + a[2]) });
+matchDataUnsorted.sort(function (a, b) { return (b[1] + b[2]) - (a[1] + a[2]) });
+
+for (let i = 0; i < missingPreGameTeams.length; i++) {
+    preGameDataUnsorted.push([missingPreGameTeams[i], 0, 0]);
+}
+
+for (let i = 0; i < missingMatchTeams.length; i++) {
+    matchDataUnsorted.push([missingMatchTeams[i], 0, 0]);
+}
+
+for (let i = 0; i < preGameDataUnsorted.length; i++) {
+    let entry = preGameDataUnsorted[i];
+    allTeamsPreGame.push(entry[0]);
+    preGameDatasets[0].data.push(entry[1]);
+    preGameDatasets[1].data.push(entry[2]);
+}
+
+for (let i = 0; i < matchDataUnsorted.length; i++) {
+    let entry = matchDataUnsorted[i];
+    allTeamsMatch.push(entry[0]);
+    matchDatasets[0].data.push(entry[1]);
+    matchDatasets[1].data.push(entry[2]);
+}
+
+
 
 function updateChartScope() {
     if ($('input[name=chart-scope]:checked').val() == 'pre-game') {
@@ -32,6 +72,7 @@ function updateChartScope() {
 
 
 function chartDispPreGameScouting() {
+    chart.data.labels = allTeamsPreGame;
     chart.data.datasets = preGameDatasets;
     chart.options.scales.xAxes[0].stacked = true;
     chart.options.scales.yAxes[0].stacked = true;
@@ -39,6 +80,7 @@ function chartDispPreGameScouting() {
 }
 
 function chartDispMatchScouting() {
+    chart.data.labels = allTeamsMatch;
     chart.data.datasets = matchDatasets;
     chart.options.scales.xAxes[0].stacked = true;
     chart.options.scales.yAxes[0].stacked = true;
@@ -59,7 +101,7 @@ var ctx = document.getElementById("chart").getContext('2d');
 var chart = new Chart(ctx, {
     type: 'horizontalBar',
     data: {
-        labels: allTeams,
+        labels: [],
         datasets: []
     },
     options: {
