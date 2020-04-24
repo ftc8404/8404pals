4f8e2aeb95d2282c61d3ded82e9b4b951914e951
Ryan Niemi <40528017+J3T4R0@users.noreply.github.com>
2020-04-24 14:02:54 -0700

    Schema registry and kafka communication

---
+++ app/refresh.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+# Delete (3) MongoDB databases, (3) Kafka topics,
+# create sample data by hitting Zuul API Gateway endpoints,
+# and return MongoDB documents as verification.
+
+# usage: python3 ./refresh.py
+
+from pprint import pprint
+from pymongo import MongoClient
+import requests
+import time
+
+client = MongoClient('mongodb://localhost:27017/')
+
+
+def main():
+    delete_databases()
+    delete_topics()
+
+    create_sample_data()
+
+    get_mongo_doc('users', 'customer.users')
+
+
+def delete_databases():
+    dbs = ['users']
+    for db in dbs:
+        client.drop_database(db)
+        print('MongoDB dropped: ' + db)
+
+    dbs = client.database_names()
+    print('Reamining databases:')
+    print(dbs)
+    print('\n')
+
+
+def delete_topics():
+    # call Kafka Manager API
+    topics = ['accounts.customer.change',
+              'users.actions.fulfill']
+    for topic in topics:
+        kafka_manager_url = 'http://localhost:9000/clusters/dev/topics/delete?t=' + topic
+        r = requests.post(kafka_manager_url, data={'topic': topic})
+        time.sleep(3)
+        print('Kafka topic deleted: ' + topic)
+    print('\n')
+
+
+def create_sample_data():
+    sample_urls = [
+        'http://localhost:8080/orders/customers/sample/users',
+        'http://localhost:8080/orders/customers/sample/fulfill',
+        'http://localhost:8080/fulfillment/fulfillments/sample/process',
+        'http://localhost:8080/fulfillment/fulfillments/sample/ship',
+        'http://localhost:8080/fulfillment/fulfillments/sample/in-transit',
+        'http://localhost:8080/fulfillment/fulfillments/sample/receive']
+
+    for sample_url in sample_urls:
+        r = requests.get(sample_url)
+        print(r.text)
+        time.sleep(5)
+    print('\n')
+
+
+def get_mongo_doc(db_name, collection_name):
+    db = client[db_name]
+    collection = db[collection_name]
+    pprint(collection.find_one())
+    print('\n')
+
+
+if __name__ == "__main__":
+    main()
