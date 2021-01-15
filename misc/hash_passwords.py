3266537ffd1e1a63b0e021821d41169dd6abb105
Alex <axchen7@outlook.com>
2021-01-15 09:56:42 -0800

    Improve has password script output formatting

--- misc/hash_passwords.py
+++ misc/hash_passwords.py
@@ -7,4 +7,5 @@ while True:
     hash = hashlib.pbkdf2_hmac(
         "sha256", password.encode('utf-8'), salt, 100000, dklen=64)
     print("hash: 0x" + hash.hex())
-    print("salt: 0x"+ salt.hex())
+    print("salt: 0x" + salt.hex())
+    print()
